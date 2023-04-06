114db332a5e4306f095db250cd4e097abf1da202
mehdi <dehghani_m@rocketmail.com>
2023-04-06 21:30:19 +0430

    finish project

--- app/src/main/java/com/example/scrollrecyclerview/activity/MainActivity.kt
+++ app/src/main/java/com/example/scrollrecyclerview/activity/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.scrollrecyclerview.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.scrollrecyclerview.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        gridHorizontalAction.setOnClickListener {
+            startActivity(Intent(this, GridHorizontalAutoScrollActivity::class.java))
+        }
+        listHorizontalAction.setOnClickListener {
+            startActivity(Intent(this, ListHorizontalAutoScrollActivity::class.java))
+        }
     }
 }
